4024346fc5f735c663cf9440a2996e2786c7412c
waiyan13 <zaw.waiyan.lwin96@gmail.com>
2025-11-08 21:51:13 +0700

    test[Landing]: Fix test error

---
+++ src/tests/landing.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router";
+import { describe, expect, it } from "vitest";
+
+import LandingPage from "@/app/pages/landing";
+
+describe("Landing page", () => {
+  it("displays heading", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]} initialIndex={0}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    expect(screen.getByText("Hire the right")).toBeInTheDocument();
+  });
+});
