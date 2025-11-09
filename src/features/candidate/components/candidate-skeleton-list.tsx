915d222686d2c94e7f49885bb2dd66fdedf5ff0a
waiyan13 <zaw.waiyan.lwin96@gmail.com>
2025-11-09 20:12:00 +0700

    refactor[Candidate]: Show heading while loading data

--- src/features/candidate/components/candidate-skeleton-list.tsx
+++ src/features/candidate/components/candidate-skeleton-list.tsx
@@ -1,3 +1,5 @@
+import { Input } from "@base-ui-components/react/input";
+
 import { CandidateCardSkelteon } from "./candidate-card-skeleton";
 
 interface CandidateSkeletonListProps {
@@ -6,10 +8,16 @@ interface CandidateSkeletonListProps {
 
 function CandidateSkeletonList({ count }: CandidateSkeletonListProps) {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-      {Array.from({ length: count }).map((_, index) => (
-        <CandidateCardSkelteon key={index} />
-      ))}
+    <div className="flex flex-col space-y-4 p-8">
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold text-2xl">Candidates</h1>
+        <Input className="w-1/5" placeholder="Search by name, email, company" />
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {Array.from({ length: count }).map((_, index) => (
+          <CandidateCardSkelteon key={index} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/features/candidate/components/candidates-list-container.tsx
+++ src/features/candidate/components/candidates-list-container.tsx
@@ -2,25 +2,37 @@ import { useMemo } from "react";
 
 import { filterCandidates } from "@/lib/util";
 
-import { CandidateCardsList } from "./candidate-cards-list";
 import { useGetCandidates } from "../hooks/useGetCandidates";
+
+import { CandidateCardsList } from "./candidate-cards-list";
 
 function CandidatesListContainer() {
   const { data } = useGetCandidates();
 
+  // index and memoize data
   const candidatesByCity = useMemo(() => {
     return data ? filterCandidates(data, "address.city") : {};
+  }, [data]);
+
+  const candidatesByName = useMemo(() => {
+    return data ? filterCandidates(data, "name") : {};
   }, [data]);
 
   const candidatesByCompany = useMemo(() => {
     return data ? filterCandidates(data, "company.name") : {};
   }, [data]);
 
+  const candidatesByEmail = useMemo(() => {
+    return data ? filterCandidates(data, "email") : {};
+  }, [data]);
+
   return (
-      <div className="flex flex-col space-y-4 p-8">
-      <h1 className="font-bold text-2xl">Candidates</h1>
-        <CandidateCardsList data={data} />
+    <div className="flex flex-col space-y-4 p-8">
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold text-2xl">Candidates</h1>
       </div>
+      <CandidateCardsList data={data} />
+    </div>
   );
 }
 
