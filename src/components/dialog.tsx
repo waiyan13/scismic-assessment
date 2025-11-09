e14fdab6e14507e8752d9b83e9afa3a50dae96a2
waiyan13 <zaw.waiyan.lwin96@gmail.com>
2025-11-10 06:00:14 +0700

    feat[Candidate]: Add modal

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -1,145 +1,145 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Dialog as BaseDialog } from "@base-ui-components/react"
-import { XIcon } from "lucide-react"
+import * as React from "react";
+import { Dialog as BaseDialog } from "@base-ui-components/react";
+import { XIcon } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/util";
 
 function Dialog({ ...props }: React.ComponentProps<typeof BaseDialog.Root>) {
-	return <BaseDialog.Root data-slot="dialog" {...props} />
+  return <BaseDialog.Root data-slot="dialog" {...props} />;
 }
 
 function DialogPortal({
-	...props
+  ...props
 }: React.ComponentProps<typeof BaseDialog.Portal>) {
-	return <BaseDialog.Portal data-slot="dialog-portal" {...props} />
+  return <BaseDialog.Portal data-slot="dialog-portal" {...props} />;
 }
 
 function DialogTrigger({
-	...props
+  ...props
 }: React.ComponentProps<typeof BaseDialog.Trigger>) {
-	return <BaseDialog.Trigger data-slot="dialog-trigger" {...props} />
+  return <BaseDialog.Trigger data-slot="dialog-trigger" {...props} />;
 }
 
 function DialogClose({
-	...props
+  ...props
 }: React.ComponentProps<typeof BaseDialog.Close>) {
-	return <BaseDialog.Close data-slot="dialog-close" {...props} />
+  return <BaseDialog.Close data-slot="dialog-close" {...props} />;
 }
 
 function DialogOverlay({
-	className,
-	...props
+  className,
+  ...props
 }: React.ComponentProps<typeof BaseDialog.Backdrop>) {
-	return (
-		<BaseDialog.Backdrop
-			data-slot="dialog-overlay"
-			className={cn(
-				"fixed inset-0 bg-black/50 transition-all duration-200 [&[data-ending-style]]:opacity-0 [&[data-starting-style]]:opacity-0",
-				className
-			)}
-			{...props}
-		/>
-	)
+  return (
+    <BaseDialog.Backdrop
+      data-slot="dialog-overlay"
+      className={cn(
+        "fixed inset-0 bg-black/50 transition-all duration-200 [&[data-ending-style]]:opacity-0 [&[data-starting-style]]:opacity-0",
+        className,
+      )}
+      {...props}
+    />
+  );
 }
 
 function DialogContent({
-	className,
-	children,
-	showCloseButton = true,
-	...props
+  className,
+  children,
+  showCloseButton = true,
+  ...props
 }: React.ComponentProps<typeof BaseDialog.Popup> & {
-	showCloseButton?: boolean
+  showCloseButton?: boolean;
 }) {
-	return (
-		<DialogPortal data-slot="dialog-portal">
-			<DialogOverlay />
-			<BaseDialog.Popup
-				data-slot="dialog-content"
-				className={cn(
-					"fixed z-50 grid w-full bg-popover text-popover-foreground sm:max-w-[calc(100%-2rem)]",
-					"gap-4 rounded-lg border p-6 shadow-lg outline-none duration-200 sm:max-w-lg sm:scale-[calc(1-0.1*var(--nested-dialogs))]",
-					"fixed bottom-0 w-full sm:top-[50%] sm:bottom-auto sm:left-[50%] sm:translate-x-[-50%] sm:translate-y-[-50%]",
-					"duration-200",
-					"data-[starting-style]:translate-y-full data-[starting-style]:opacity-0",
-					"data-[ending-style]:translate-y-full data-[ending-style]:opacity-0",
-					"data-[starting-style]:sm:translate-y-[-50%] data-[starting-style]:sm:scale-95",
-					"data-[ending-style]:sm:translate-y-[-50%] data-[ending-style]:sm:scale-95",
-					className
-				)}
-				{...props}
-			>
-				{children}
-				{showCloseButton && (
-					<DialogClose className="absolute top-4 right-4 rounded-xs text-muted-foreground opacity-50 ring-offset-popover transition-opacity hover:opacity-100 focus:outline-hidden focus:ring-[3px] focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0">
-						<XIcon />
-						<span className="sr-only">Close</span>
-					</DialogClose>
-				)}
-			</BaseDialog.Popup>
-		</DialogPortal>
-	)
+  return (
+    <DialogPortal data-slot="dialog-portal">
+      <DialogOverlay />
+      <BaseDialog.Popup
+        data-slot="dialog-content"
+        className={cn(
+          "fixed z-50 grid w-full bg-popover text-popover-foreground sm:max-w-[calc(100%-2rem)]",
+          "gap-4 rounded-lg border p-6 shadow-lg outline-none duration-200 sm:max-w-lg sm:scale-[calc(1-0.1*var(--nested-dialogs))]",
+          "fixed bottom-0 w-full sm:top-[50%] sm:bottom-auto sm:left-[50%] sm:translate-x-[-50%] sm:translate-y-[-50%]",
+          "duration-200",
+          "data-[starting-style]:translate-y-full data-[starting-style]:opacity-0",
+          "data-[ending-style]:translate-y-full data-[ending-style]:opacity-0",
+          "data-[starting-style]:sm:translate-y-[-50%] data-[starting-style]:sm:scale-95",
+          "data-[ending-style]:sm:translate-y-[-50%] data-[ending-style]:sm:scale-95",
+          className,
+        )}
+        {...props}
+      >
+        {children}
+        {showCloseButton && (
+          <DialogClose className="absolute top-4 right-4 rounded-xs text-muted-foreground opacity-50 ring-offset-popover transition-opacity hover:opacity-100 focus:outline-hidden focus:ring-[3px] focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0">
+            <XIcon />
+            <span className="sr-only">Close</span>
+          </DialogClose>
+        )}
+      </BaseDialog.Popup>
+    </DialogPortal>
+  );
 }
 
 function DialogHeader({ className, ...props }: React.ComponentProps<"div">) {
-	return (
-		<div
-			data-slot="dialog-header"
-			className={cn("flex flex-col gap-2 text-center sm:text-left", className)}
-			{...props}
-		/>
-	)
+  return (
+    <div
+      data-slot="dialog-header"
+      className={cn("flex flex-col gap-2 text-center sm:text-left", className)}
+      {...props}
+    />
+  );
 }
 
 function DialogFooter({ className, ...props }: React.ComponentProps<"div">) {
-	return (
-		<div
-			data-slot="dialog-footer"
-			className={cn(
-				"flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
-				className
-			)}
-			{...props}
-		/>
-	)
+  return (
+    <div
+      data-slot="dialog-footer"
+      className={cn(
+        "flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
+        className,
+      )}
+      {...props}
+    />
+  );
 }
 
 function DialogTitle({
-	className,
-	...props
+  className,
+  ...props
 }: React.ComponentProps<typeof BaseDialog.Title>) {
-	return (
-		<BaseDialog.Title
-			data-slot="dialog-title"
-			className={cn("font-semibold text-lg leading-none", className)}
-			{...props}
-		/>
-	)
+  return (
+    <BaseDialog.Title
+      data-slot="dialog-title"
+      className={cn("font-semibold text-lg leading-none", className)}
+      {...props}
+    />
+  );
 }
 
 function DialogDescription({
-	className,
-	...props
+  className,
+  ...props
 }: React.ComponentProps<typeof BaseDialog.Description>) {
-	return (
-		<BaseDialog.Description
-			data-slot="dialog-description"
-			className={cn("text-muted-foreground text-sm", className)}
-			{...props}
-		/>
-	)
+  return (
+    <BaseDialog.Description
+      data-slot="dialog-description"
+      className={cn("text-muted-foreground text-sm", className)}
+      {...props}
+    />
+  );
 }
 
 export {
-	Dialog,
-	DialogPortal,
-	DialogOverlay,
-	DialogClose,
-	DialogTrigger,
-	DialogContent,
-	DialogHeader,
-	DialogFooter,
-	DialogTitle,
-	DialogDescription,
-}
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+};
--- src/features/candidate/components/candidate-cards-list.tsx
+++ src/features/candidate/components/candidate-cards-list.tsx
@@ -7,8 +7,17 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/dialog";
 
 import type { Candidate } from "@/types/candidate";
+import { Input } from "@base-ui-components/react";
+import { Label } from "@/components/label";
 
 interface CandidateCardProps {
   data: Candidate[];
@@ -18,37 +27,83 @@ function CandidateCardsList({ data }: CandidateCardProps) {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
       {data.map((candidate) => (
-        <motion.div
-          key={candidate.id}
-          whileHover={{
-            scale: [null, 1.03],
-          }}
-          transition={{
-            duration: 0.3,
-            ease: "easeOut",
-          }}
-        >
-          <Card className="border-none">
-            <CardHeader>
-              <CardTitle className="text-secondary text-xl">{candidate.name}</CardTitle>
-              <CardDescription className="text-primary-500">
-                @{candidate.username}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className="grid grid-cols-[20px_1fr] gap-2">
-                <Briefcase color="#4a5565" />
-                <span className="pl-2">{candidate.company.name}</span>
+        <Dialog>
+          <DialogTrigger>
+            <motion.div
+              key={candidate.id}
+              whileHover={{
+                scale: [null, 1.03],
+              }}
+              transition={{
+                duration: 0.3,
+                ease: "easeOut",
+              }}
+            >
+              <Card className="border-none">
+                <CardHeader>
+                  <CardTitle className="text-secondary text-xl">
+                    {candidate.name}
+                  </CardTitle>
+                  <CardDescription className="text-primary-500">
+                    @{candidate.username}
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <div className="grid grid-cols-[20px_1fr] gap-2">
+                    <Briefcase color="#4a5565" />
+                    <span className="pl-2">{candidate.company.name}</span>
 
-                <MapPin color="#4a5565" />
-                <span className="pl-2">{candidate.address.city}</span>
+                    <MapPin color="#4a5565" />
+                    <span className="pl-2">{candidate.address.city}</span>
 
-                <Phone color="#4a5565" />
-                <span className="pl-2">{candidate.phone}</span>
+                    <Phone color="#4a5565" />
+                    <span className="pl-2">{candidate.phone}</span>
+                  </div>
+                </CardContent>
+              </Card>
+            </motion.div>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>{candidate.name}</DialogTitle>
+            </DialogHeader>
+            <div className="flex flex-col space-y-4">
+              <div className="flex flex-col space-y-2">
+                <Label>Email</Label>
+                <Input
+                  className="w-full border-none"
+                  value={candidate.email}
+                  readOnly
+                />
               </div>
-            </CardContent>
-          </Card>
-        </motion.div>
+              <div className="flex flex-col space-y-2">
+                <Label>Address</Label>
+                <Input
+                  className="w-full border-none"
+                  value={`${candidate.address.suite}, ${candidate.address.street},${candidate.address.city}`}
+                  readOnly
+                />
+              </div>
+              <p className="font-bold text-lg">Company Info</p>
+              <div className="flex flex-col space-y-2">
+                <Label>Name</Label>
+                <Input
+                  className="w-full border-none"
+                  value={candidate.company.name}
+                  readOnly
+                />
+              </div>
+              <div className="flex flex-col space-y-2">
+                <Label>Catchphrase</Label>
+                <Input
+                  className="w-full border-none"
+                  value={candidate.company.catchPhrase}
+                  readOnly
+                />
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
       ))}
     </div>
   );
