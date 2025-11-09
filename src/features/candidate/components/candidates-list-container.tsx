2a923c6f0ec7803ff39b3f73183644f5a4cebf3a
waiyan13 <zaw.waiyan.lwin96@gmail.com>
2025-11-09 20:23:11 +0700

    feat[Search]: Attach react-hook-form to search input

--- src/features/candidate/components/candidates-list-container.tsx
+++ src/features/candidate/components/candidates-list-container.tsx
@@ -1,13 +1,30 @@
 import { useMemo } from "react";
 
+import { useForm } from "react-hook-form";
+
+import { Form, FormControl, FormField, FormItem } from "@/components/form";
+import { Input } from "@/components/input";
 import { filterCandidates } from "@/lib/util";
 
 import { useGetCandidates } from "../hooks/useGetCandidates";
 
 import { CandidateCardsList } from "./candidate-cards-list";
 
+interface SearchForm {
+  search: string;
+}
+
 function CandidatesListContainer() {
   const { data } = useGetCandidates();
+
+  const form = useForm<SearchForm>({
+    defaultValues: {
+      search: "",
+    },
+  });
+
+  const { watch } = form;
+  const searchTerm = watch("search");
 
   // index and memoize data
   const candidatesByCity = useMemo(() => {
@@ -28,8 +45,28 @@ function CandidatesListContainer() {
 
   return (
     <div className="flex flex-col space-y-4 p-8">
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col space-y-4 md:flex-row md:items-center md:justify-between">
         <h1 className="font-bold text-2xl">Candidates</h1>
+
+        <Form {...form}>
+          <form className="w-full md:w-1/5" id="search-candidate">
+            <FormField
+              name="search"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      className="w-full"
+                      placeholder="Search by name, email, company"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </div>
       <CandidateCardsList data={data} />
     </div>
